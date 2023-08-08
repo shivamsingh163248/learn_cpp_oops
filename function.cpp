86d00b6bee25d673da69dce17ebf1499d5b705b8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-09 02:07:01 +0530

    changed

---
+++ function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// using the name  space for the avoding the conflict between the two identifires 
+
+using namespace std ;
+
+// now creating main function 
+
+int  main()
+{
+    /* code */
+
+  cout << "shivam singh" ; 
+  int a = 10 ; 
+  int b = 20 ; 
+  int c = a+b ; 
+  cout << c ; 
+
+
+    return 0;
+}
+
+
+
+
