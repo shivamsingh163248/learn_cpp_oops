b0f49c78268ae1531d5c5ba45d1ae44de7267abf
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-09 15:59:44 +0530

    changed

---
+++ returnbyrefrence.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ; 
+
+// conw creating the functuin 
+void cheking(int a , int b){
+    // creating the pprint function 
+    cout<<"name is a"<<a   ; 
+}
+
+// now creating the main function 
+int main (){
+
+
+    // calling the function 
+    cheking(34 , 56) ; 
+    return 0 ; 
+}
+
