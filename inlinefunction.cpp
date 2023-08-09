b46dea118b346248a0b572f379c02245c90f1122
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-10 03:23:44 +0530

    learning the inline function in c++

---
+++ inlinefunction.cpp
@@ -0,0 +1,34 @@
+// now creating the inline function 
+// creating the prepocess and then library 
+#include <iostream>
+// now creating the standerd name space 
+using namespace std ; 
+
+// now creating the inline function 
+inline void greaters (int a , int b){
+
+    // creating the unary operator for the checking 
+ int min = (a>b)?a:b ; 
+ // check another for the return 
+ int     shi  = (a<b)? 45:22 ; 
+ // now agian creating the trianaty opreator 
+  int     iti  = (a>b)? 45:22 ; 
+
+ // now check for the printing if it is the correct then printing the value 
+
+ cout<< min << endl  ;
+ cout<< shi << endl  ;
+ cout<< iti << endl  ;
+
+  
+ 
+}
+
+// now creating the main function 
+int main (){
+
+    // now calling the inline function 
+    greaters(20,40) ; 
+    return 0 ; 
+
+}
