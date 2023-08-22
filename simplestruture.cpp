d91ee61940746254794d8069901947ad16f0d142
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-22 15:10:04 +0530

    strecture of the simple function

---
+++ simplestruture.cpp
@@ -0,0 +1,26 @@
+#include <iostream> // this is the header file 
+// now writing the statnderd name space 
+using namespace std ;  // this is the standerd  name space 
+
+// niw creating the fucntion 
+
+int main()
+{
+    
+  // now writing the statement 
+
+  int a  = 10; 
+  int b  = 20 ; 
+
+  // creating the add 
+  int c = a+b ; 
+
+  // now printng the fucntion 
+  cout<<c << endl ; 
+
+
+
+
+    return 0;
+}
+
