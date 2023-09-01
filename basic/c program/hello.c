bfae8027aaab9994d814c2eba80b74685ac5e937
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-01 23:01:53 +0530

    changed

--- basic/c program/hello.c
+++ basic/c program/hello.c
@@ -4,10 +4,19 @@
 int main()
 {
    
-int a = 10 ; 
-int b = 20 ; 
-int c = a * b ; 
+int a ; 
+int b ; 
+int c ; 
 
+printf("sunial take the input for a ") ; 
+scanf("%d" , &a) ; 
+printf(" yogesh take the input for the b ") ; 
+scanf("%d" , &b) ; 
+ 
+c = a+b ; 
+printf(" now the ans for the kishan ");
 printf("%d" , c) ; 
+
+
     return 0;
 }
