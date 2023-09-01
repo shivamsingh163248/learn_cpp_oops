f74a1fb89fe57701433565135b23f73cc76091df
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-01 23:21:47 +0530

    changed

---
+++ basic/c program/areacircle.c
@@ -0,0 +1,28 @@
+// pripocess 
+#include<stdio.h>
+#include<conio.h>
+
+// now writing the main function 
+int main()
+{
+    
+ float a ; 
+ int r ; 
+ float p  = 3.14; 
+ char s  ; 
+
+ printf("enter the value of the redius \n") ; 
+ scanf("%d" , &r) ; 
+ printf("now the print the are of the circle\n ") ; 
+ a = r*r*p ; 
+ printf("%f" , a) ; 
+ printf("enter yout name \n");
+ scanf("%c" , &s) ; 
+ printf("your name is %c" , s) ; 
+
+
+
+
+
+    return 0;
+}
